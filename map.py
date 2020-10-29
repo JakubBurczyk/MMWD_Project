133d23c75042fdd4ba86dbf044996f6f1a1f9dd1
Jakub Burczyk <jburczyk99@gmail.com>
2020-10-29 17:34:37 +0100

    Edge weight randomization

--- map.py
+++ map.py
@@ -6,31 +6,48 @@ import random
 class Map:
     min_edg_factor = 3 / 2
     max_edg_factor = 5 / 2
+    is_charger = 'is_charger'
+    weight = 'weight'
+
+    min_weight = 10
+    max_weight = 20
 
     def __init__(self, size: int, edg: int = 0):
-        if edg < size*3/2:
-            edg = random.randrange(int(size*self.min_edg_factor), int(size*self.max_edg_factor))
+        if edg < size*self.min_edg_factor:
+            edg = random.randint(size*self.min_edg_factor, size*self.max_edg_factor)
 
-        self.edg = edg
+        self.edg_number = edg
         self.G = nx.gnm_random_graph(size, edg)
 
-        #test charger as last node
-        self.chargers = [size-1]
+        #initialize random weights in range <min_weight, max_weight>
 
-    def print(self):
-        e = [(u, v) for (u, v, d) in self.G.edges(data=True)]
+        for (u, v) in self.G.edges():
+            e = self.G.edges[u, v]
+            self.G.edges[u, v][self.weight] = random.randint(self.min_weight, self.max_weight)
+
+        #intialize charger attribute
+        nx.set_node_attributes(self.G, False, self.is_charger)
+
+        #test charger as last node
+        self.G.nodes[size-1][self.is_charger] = True
+
+    def print(self, edge_filter=0):
+        if edge_filter < self.min_weight:
+            edge_filter = self.min_weight
+
+        e = [(u, v) for (u, v, d) in self.G.edges(data=True) if d[self.weight] > edge_filter]
 
         pos = nx.spring_layout(self.G)  # positions for all nodes
 
         # nodes
         color_map = []
-        for node in self.G:
-            if node in self.chargers:
+        for node in self.G.nodes(data=True):
+            if node[1][self.is_charger] is True:
                 color_map.append('lightgreen')
             else:
                 color_map.append('lightblue')
 
-        nx.draw_networkx_nodes(self.G, pos, node_size=200, node_color = color_map)
+        nx.draw_networkx_nodes(self.G, pos, node_size=200, node_color=color_map)
 
         # edges
         nx.draw_networkx_edges(self.G, pos, edgelist=e, width=1)
