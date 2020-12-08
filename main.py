b9bdec9160c0915e8825935d247adf478f302638
Jakub Burczyk <jburczyk99@gmail.com>
2020-12-08 15:28:30 +0100

    Constructor and output overhaul 1.2

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import map
 import vehicle
 import genetics
 
-vehicles_number = 20
+vehicles_number = 100
 cycles = 200
 
 mapa = map.Map(size=100, as_complete=False, tries=10000)
 mapa.print()
 
-gen1 = genetics.Genetics(mapa=mapa, vehicles_no=vehicles_number, cycles_number=cycles, slicing_type=genetics.SlicingType.START_TO_RANDOM,show_best=True, show_progress=True, plot_all=True, show_map_solution=False)
+gen1 = genetics.Genetics(mapa=mapa, vehicles_no=vehicles_number, cycles_number=cycles, slicing_type=genetics.SlicingType.VISITED_EPSILON,show_best=True, show_progress=True, plot_all=True, show_map_solution=False)
 # gen2 = genetics.Genetics(mapa=mapa, vehicles_no=vehicles_number, cycles_number=cycles, slicing_type=genetics.SlicingType.START_TO_RANDOM,show_best=True, show_progress=True, plot_all=False, show_map_solution=False)
 
 
