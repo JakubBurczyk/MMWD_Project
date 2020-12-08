a37aab583ef126923d60638ad3abcb4652212ca5
Jakub Burczyk <jburczyk99@gmail.com>
2020-12-08 13:39:08 +0100

    Constructor and start overhaul

--- genetics.py
+++ genetics.py
@@ -5,18 +5,25 @@ import map
 import vehicle
 import random
 from operator import attrgetter
+from enum import Enum
+
+
+class SlicingType(Enum):
+    VISITED_EPSILON = 0
+    START_TO_RANDOM = 1
 
 
 class Genetics:
 
-    def __init__(self, nodes: int = 0, edges: int = 0, mapa: map.Map = None, vehicles_no: int = 99):
-
+    def __init__(self, nodes: int = 0, edges: int = 0, mapa: map.Map = None, vehicles_no: int = 100,cycles_number = 100, slicing_type = SlicingType.VISITED_EPSILON):
+        self.cycles_number = cycles_number
         self.avgerage_ages = []
         self.done_cycles = 0
         self.charger_nums_of_best = []
         self.kilometrages_of_best = []
         self.visited_nodes_num_of_best = []
         self.nodes_to_chargers_ratios_of_best = []
+        self.slicing_type = slicing_type
 
         if mapa is None:
             self.mapa = map.Map(nodes, edges, as_complete=False, tries=10000)
@@ -31,6 +38,29 @@ class Genetics:
         while self.get_vehicles_number() % 4:
             self.vehicles.append(vehicle.Vehicle(self.mapa))
 
+        self.compute_solution(show_best=True,show_progress=True,plot_all=False)
+
+    def compute_solution(self, show_best = True,show_progress = False, plot_all = False):
+        print("----START----")
+        for i in range(self.cycles_number):
+            self.cycle()
+
+            if show_progress and i % int(self.cycles_number/3) == 0:
+                print("Cycle: ", i+1, " out of: ", self.cycles_number)
+
+        # self.charger_nums_of_best .append(len(self.vehicles[0].chargers))
+
+        if show_best:
+            self.vehicles[0].print_status()
+
+        if plot_all:
+            self.plot_avg_age()
+            self.plot_charger_nums_of_best()
+            self.plot_kilometrages_of_best()
+            self.plot_visited_nodes_num_of_best()
+            self.plot_nodes_to_chargers_ratios_of_best()
+
+        print("----DONE----")
     def cycle(self):
         #FIXME
         self.QUICKFIX_visited_and_chargers_doubles()
@@ -139,8 +169,9 @@ class Genetics:
     def rank(self):
         for v in self.vehicles:
             v.visited_nodes_num = len(v.visited_nodes)
-           # v.nodes_to_chargers_ratio = v.visited_nodes_num/len(v.chargers)
+            # v.nodes_to_chargers_ratio = v.visited_nodes_num/len(v.chargers)
             v.nodes_to_chargers_ratio = len(v.chargers) / v.visited_nodes_num
+
         # self.vehicles.sort(key=attrgetter('kilometrage'), reverse=True)
         # self.vehicles.sort(key=attrgetter('visited_nodes_num'), reverse=True)
         self.vehicles.sort(key=attrgetter('nodes_to_chargers_ratio'), reverse=False)
@@ -204,14 +235,22 @@ class Genetics:
     def get_gene_binary_slice(self, v: vehicle.Vehicle):
         gene_nodes = [0] * self.mapa.size
 
-        for i in range(self.mapa.size):
-            if i in v.visited_nodes:
-                gene_nodes[i] = 1
-                if i > 0:
-                    gene_nodes[i - 1] = 1
+        if self.slicing_type == SlicingType.VISITED_EPSILON:
+            slice_epsilon = 2
+            for i in range(self.mapa.size):
+                if i in v.visited_nodes:
+                    gene_nodes[i] = 1
+                    if i >= slice_epsilon:
+                        for j in range(1,slice_epsilon+1):
+                            gene_nodes[i - j] = 1
+                    if i < self.mapa.size - slice_epsilon:
+                        for j in range(1, slice_epsilon + 1):
+                            gene_nodes[i + j] = 1
 
-                if i < self.mapa.size - 1:
-                    gene_nodes[i + 1] = 1
+        elif self.slicing_type == SlicingType.START_TO_RANDOM:
+            slice_point = random.randint(0,len(gene_nodes))
+            for i in range(0,slice_point):
+                gene_nodes[i]= 1
 
         return gene_nodes
 
--- main.py
+++ main.py
@@ -2,40 +2,31 @@ import map
 import vehicle
 import genetics
 
-# m1 = map.Map(20, as_complete=False,tries=100)
-# m1.print()
-# print(m1.get_distance_between(0, 1))
-# print("Chargers:")
-# print(m1.count_chargers())
-#
-# v1 = vehicle.Vehicle(m1)
-# v2 = vehicle.Vehicle(m1)
-#
-# v1.print_occupied_IDs()
-#
-#
-# v1.print_status()
-# v2.print_status()
+vehicles_number = 50
+cycles = 200
 
-gen = genetics.Genetics(nodes=100, edges=0, vehicles_no=50)
-# print(gen.mapa.neighbours(2))
+mapa = map.Map(100, 0, as_complete=False, tries=10000)
+mapa.print()
 
-gen.mapa.print()
+gen1 = genetics.Genetics(mapa=mapa, vehicles_no=vehicles_number, slicing_type=genetics.SlicingType.VISITED_EPSILON)
+gen2 = genetics.Genetics(mapa=mapa, vehicles_no=vehicles_number, slicing_type=genetics.SlicingType.VISITED_EPSILON)
 
-for i in range(200):
-    print("-----------------CYCLE NUMBER ", i + 1, "-----------------")
-    gen.cycle()
-    for v in gen.vehicles:
-        pass
-        #v.print_status()
-    #gen.vehicles[0].print_status();
+#gen.mapa.print()
 
-gen.vehicles[0].print_status()
-gen.plot_avg_age()
-gen.plot_charger_nums_of_best()
-gen.plot_kilometrages_of_best()
-gen.plot_visited_nodes_num_of_best()
-gen.plot_nodes_to_chargers_ratios_of_best()
+# for i in range(200):
+#     print("-----------------CYCLE NUMBER ", i + 1, "-----------------")
+#     gen.cycle()
+#     for v in gen.vehicles:
+#         pass
+#         #v.print_status()
+#     #gen.vehicles[0].print_status();
+#
+# gen.vehicles[0].print_status()
+# gen.plot_avg_age()
+# gen.plot_charger_nums_of_best()
+# gen.plot_kilometrages_of_best()
+# gen.plot_visited_nodes_num_of_best()
+# gen.plot_nodes_to_chargers_ratios_of_best()
 
 print("EO MAIN")
-m1.print()
+
